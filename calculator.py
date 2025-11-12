ecf744929355964a3ab1e55d4022ad1ddcf90004
Angel Reyes <reyesangel466@gmail.com>
2025-11-12 14:50:49 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -5,6 +5,20 @@ calculator.py
 One function per operation, in order.
 """
 import math
+def square_root(num):
+    try:
+        if num < 0:
+            raise ValueError("Cannot take the square root of a negative number.")
+        return math.sqrt(num)
+    except ValueError as e:
+        print(f"Error: {e}")
+        raise
+def hypot(num1, num2):
+    try:
+        return math.hypot(num1, num2)
+    except Exception as e:
+        print(f"Error Calculating Hypotent: {e}")
+        raise
 # First example
 def add(a, b):
     return a + b
